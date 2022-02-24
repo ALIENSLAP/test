7c5144c92c38adc1b0c5ada222848855fbc9fa2f
lap <lap17328754440@163.com>
2022-02-24 17:53:04 +0800

    master second commit

--- src/main/test.java
+++ src/main/test.java
@@ -4,6 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("mast commit");
         System.out.println("master commit");
+        System.out.println("git push");
 
         }
 
